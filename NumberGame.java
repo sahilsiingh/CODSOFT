b48d61f0e35318cd0a76a4ccdf40dfa64713108a
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-11-21 13:07:09 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGame {
+public class NumberGame { 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
